5f9f3c1a70421412f14f7234fb75882cb5246f49
Stephanie5Z <165931720+Stephanie5Z@users.noreply.github.com>
2024-04-03 19:47:36 +0300

    Update main.java
    Java lesson Nr. 5

--- main.java
+++ main.java
@@ -1,1 +1,25 @@
+public class Main {
+  public static void main(String[] args) {
+//seasons: winter, spring, summer, fall
+//clothes: jacket, sweater, shirt, pants
+    String season = "apple";
 
+    if (season == "winter") {
+      System.out.println("Wear a jacket.");
+    }
+    else if (season == "spring") {
+      System.out.println("Wear a sweater.");
+    }
+    else if (season == "summer") {
+      System.out.println("Wear a t-shirt.");
+    }
+    else if (season == "fall") {
+      System.out.println("Wear a pants.");
+    }
+    else {
+      System.out.println("Invalid season.");
+    }
+  }
+
+
+}
