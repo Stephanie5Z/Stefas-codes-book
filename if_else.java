6f3742fef1c0fdcf6128767a69fbc769b7bd2526
Stephanie5Z <165931720+Stephanie5Z@users.noreply.github.com>
2024-04-04 18:08:04 +0300

    Update if_else.java

--- if_else.java
+++ if_else.java
@@ -1,25 +1,42 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
 public class Main {
   public static void main(String[] args) {
-//seasons: winter, spring, summer, fall
-//clothes: jacket, sweater, shirt, pants
-    String season = "apple";
+    // System.out.println("Hello world!");
+    // winter, spring, summer, autumn
+    // warm jacket, t-shirt, swimming suite, rain coat
+    String season = "qwerty";
 
     if (season == "winter") {
-      System.out.println("Wear a jacket.");
+      System.out.println("Wear a warm jacket!");
+    } else if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    } else if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    } else if (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    } else {
+      System.out.println("I do not recongnize this season!");
     }
-    else if (season == "spring") {
-      System.out.println("Wear a sweater.");
+
+    double temp = -10.23;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    } else if (temp <= 15) {
+      System.out.println("Wear warm");
+    } else if (temp <= 30) {
+      System.out.println("Wear normal");
+    } else {
+      System.out.println("You need cooling");
     }
-    else if (season == "summer") {
-      System.out.println("Wear a t-shirt.");
-    }
-    else if (season == "fall") {
-      System.out.println("Wear a pants.");
-    }
-    else {
-      System.out.println("Invalid season.");
-    }
+
   }
 
-
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
 }
